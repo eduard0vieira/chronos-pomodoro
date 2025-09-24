501db4ae953782599e120aa238077d4dc0da2277
eduard0vieira <eduardohvieira05@gmail.com>
2025-09-24 00:59:58 -0300

    feat: Add Cycles component and add them to MainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,10 +1,18 @@
 import styles from './styles.module.css';
 import DefaultInput from '../DefaultInput';
+import Cycle from '../Cycles';
 
 export default function MainForm() {
   return (
     <div className={styles.container}>
-      <DefaultInput labelText='Task' type='text' id='task' placeholder='Digite Algo'/>
+      <DefaultInput
+        labelText='Task'
+        type='text'
+        id='task'
+        placeholder='Digite Algo'
+      />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Cycle />
     </div>
-  )
+  );
 }
