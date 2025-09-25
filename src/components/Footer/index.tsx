41581bafa6f0a36e82738d10737dec77358f4485
eduard0vieira <eduardohvieira05@gmail.com>
2025-09-24 22:46:58 -0300

    refactor: refactor footer structure and add light theme vars

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,20 +2,22 @@ import styles from './styles.module.css';
 
 export default function Footer() {
   return (
-    <div className={styles.container}>
-      <a className={styles.link} href='/'>
-        Entenda como funciona a técnica pomodoro
-      </a>
-      <span className={styles.github}>
-        <span>Chronos Pomodoro ©{new Date().getFullYear()} - feito por</span>
-        <a
-          className={styles.link}
-          href='https://github.com/eduard0vieira'
-          target='_blank'
-        >
-          @eduard0vieira
+    <footer>
+      <div className={styles.container}>
+        <a className={styles.link} href='/'>
+          Entenda como funciona a técnica pomodoro
         </a>
-      </span>
-    </div>
+        <span className={styles.github}>
+          <span>Chronos Pomodoro ©{new Date().getFullYear()} - feito por</span>
+          <a
+            className={styles.link}
+            href='https://github.com/eduard0vieira'
+            target='_blank'
+          >
+            @eduard0vieira
+          </a>
+        </span>
+      </div>
+    </footer>
   );
 }
